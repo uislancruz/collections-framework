e8eebf6cb093c159a1a15dd2a714390ba4d5f4ae
Uislan Cruz <uislansk8@hotmail.com>
2026-05-06 10:56:17 -0300

    Usando listas do tipo ArrayList

--- src/com/algaworks/agencia/CadastroHotel.java
+++ src/com/algaworks/agencia/CadastroHotel.java
@@ -1,4 +1,16 @@
 package com.algaworks.agencia;
 
+import java.util.ArrayList;
+
 public class CadastroHotel {
+
+    private final ArrayList hoteis = new ArrayList(1000);
+
+    public void adicionar(String nome, String cidade, double precoDiaria) {
+        hoteis.add(new Hotel(nome, cidade, precoDiaria));
+    }
+
+    public ArrayList obterTodos(){
+        return hoteis;
+    }
 }
